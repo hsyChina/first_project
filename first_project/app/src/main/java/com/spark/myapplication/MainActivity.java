b03c0f1d550ae0fe9b59c2b46543980d317a80cc
hsyChina <hsy810676934>
2021-07-31 14:21:57 +0800

    主页面土司控制

--- first_project/app/src/main/java/com/spark/myapplication/MainActivity.java
+++ first_project/app/src/main/java/com/spark/myapplication/MainActivity.java
@@ -3,12 +3,23 @@ package com.spark.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView one;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        one = findViewById(R.id.one);
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"version,控制",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
